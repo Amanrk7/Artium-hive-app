f64d1ca1812329d9a415cf75f5227a212f46bf3f
Amanrk7 <amanrk0011@gmail.com>
2025-01-26 22:44:53 +0530

    updated repo

---
+++ src/components/Footer.jsx
@@ -0,0 +1,15 @@
+export const Footer = () => {
+  return (
+    <div className="footer">
+      <div className="footer-child1">
+        <div className="footer-child1-2">MAYA INCOPRATION</div>
+        <div className="footer-child1-1">
+          HNO. 303 GANESH GALAXY CITY BUILDISNG AYODHYA BYPASS ROAD BHOPAL
+          462041Dist.-BHOPAL
+        </div>
+      </div>
+
+      <div className="footer-child2">Artium Hive, all rights reserved</div>
+    </div>
+  );
+};
